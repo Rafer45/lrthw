f655c1d533a3bd8b0d342647c9f6a2dd2baccf7a
Rafer45 <ataque992@hotmail.com>
2015-12-16 11:04:27 -0600

    Complete exercise 16

--- LRTHW/ex16.rb
+++ LRTHW/ex16.rb
@@ -9,4 +9,26 @@ $stdin.gets
 puts "Opening the file..."
 target = open(filename, 'w')
 
-puts 
+puts "Truncating the file. Goodbye!"
+target.truncate(0)
+
+puts "Now I'm going to ask you for three lines."
+
+print "line 1: "
+line1 = $stdin.gets.chomp
+print "line 2: "
+line2 = $stdin.gets.chomp
+print "line 3: "
+line3 = $stdin.gets.chomp
+
+puts "I'm going to write these to the file."
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+puts "And finally, we close it."
+target.close
